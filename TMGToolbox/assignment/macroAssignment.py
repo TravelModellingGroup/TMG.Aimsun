48a827f0dbc666a5b71ba0a05c1f14f8d357641c
Brendan Reilly <brendan.reilly@utoronto.ca>
2021-02-12 11:34:42 -0500

    Reverted macroAssigment.py

--- TMGToolbox/assignment/macroAssignment.py
+++ TMGToolbox/assignment/macroAssignment.py
@@ -5,35 +5,31 @@ from PyMacroKernelPlugin import *
 from PyMacroToolPlugin import *
 from PyMacroAdjustmentPlugin import *
 from PyMacroPTPlugin import *
+from PyFrankWolfePlugin import *
 import sys
 import os
+sys.path.append( xtmf_parameters['toolboxPath'] )
+from common import utilities as _util
 
-sys.path.append(xtmf['toolboxPath'])
-import utilities as _util
 
 system = GKSystem.getSystem()
 model = system.getActiveModel()
-print
-'got model'
-cmd = model.createNewCmd(model.getType("MacroScenario"))
-model.getCommander().addCommand(cmd)
+print 'got model'
+cmd = model.createNewCmd( model.getType( "MacroScenario" ))
+model.getCommander().addCommand( cmd )
 scenario = cmd.createdObject()
 scenario.setDemand(model.getCatalog().find(int(xtmf_parameters["matrix"])))
-print
-'created scenario %d' % int(scenario.getId())
+print 'created scenario %d' % int(scenario.getId())
 
-cmd1 = model.createNewCmd(model.getType("GKPathAssignment"))
-model.getCommander().addCommand(cmd1)
+cmd1 = model.createNewCmd( model.getType( "GKPathAssignment" ))
+model.getCommander().addCommand( cmd1 )
 PathAssignment = cmd1.createdObject()
-print
-'created path assignment %d in %s with the name of %s' % (
-	int(PathAssignment.getId()), str(PathAssignment.getFolderPath()), str(PathAssignment.getFileName()))
+print 'created path assignment %d in %s with the name of %s' % (int(PathAssignment.getId()), str( PathAssignment.getFolderPath()), str(PathAssignment.getFileName()))
 
-inputData = scenario.getOutputData()
-inputData.setGenerateSkims(1)
-inputData.setActivatePathStatistics(True)
+dataToOutput = scenario.getOutputData()
+dataToOutput.setGenerateSkims(1)
 
-scenario.setOutputData(inputData)
+scenario.setOutputData(dataToOutput)
 
 '''cmd2 = model.createNewCmd( model.getType( "MacroExperiment" ))
 cmd2.setScenario( scenario )
@@ -42,38 +38,49 @@ experiment = cmd2.createdObject()
 experiment.setEngine('FrankeWolfe')
 experiment.setOutputPathAssignment(PathAssignment)'''
 
-experiment = GKSystem.getSystem().newObject("MacroExperiment", model)
+'''experiment = GKSystem.getSystem().newObject( "MacroExperiment", model )
 experiment.setEngine("FrankWolfe")
 params = experiment.createParameters()
-params.setMaxIterations(50)
-params.setMaxRelativeGap(0.001)
+params.setMaxIterations ( 50 )
+params.setMaxRelativeGap ( 0.001 )
+params.setFrankWolfeMethod ( 0 )
+experiment.setParameters( params ) '''
 
-params.setFrankWolfeMethod(0)
-experiment.setParameters(params)
-experiment.setScenario(scenario)
-experiment.setOutputPathAssignment(PathAssignment)
-model.getCatalog().add(experiment)
-print
-"created experiment with id %d" % int(experiment.getId())
+experiment = GKSystem.getSystem().newObject( "MacroExperiment", model )
+experiment.setEngine( "FrankWolfe" )
+params = experiment.createParameters()
+params.setMaxIterations ( 50 )
+params.setMaxRelativeGap ( 0.001 )
+params.setFrankWolfeMethod ( CFrankWolfeParams.eNormal )
+experiment.setParameters( params )
 
-system.executeAction("execute", experiment, [], "static assignment")
+
+model.getCatalog().add( experiment )
+
+print "created experiment with id %d" %int(experiment.getId())
+
+experiment.setScenario( scenario )
+experiment.setOutputPathAssignment( PathAssignment )
+
+
+system.executeAction( "execute", experiment, [], "static assignment")
+experiment.getStatsManager().createTrafficState()
 
 skims = experiment.getOutputData().getSkimMatrices(model)
 for skim in skims:
-	name = "D:\Users\Bilal\Documents\GTA_AIMSUN\\" + str(skim.getId()) + "_" + str(
-		skim.getVehicle().getId()) + "_" + str(skim.getVehicle().getName()) + "_" + str(
-		skim.getFrom().toString()) + "_" + str(skim.getDuration().toString()) + ".csv"
-	print
-	name
-	_util.exportMatrixCSV(name, skim)
+    def convert_to_int(time):
+        time = time.split(':')
+        return str(time[0])+str(time[1])+str(time[2])
+    int_start_time = convert_to_int(skim.getFrom().toString())
+    int_duration_time = convert_to_int(skim.getDuration().toString())
+    name = "D:\Users\Bilal\Documents\GTA_AIMSUN\\" + str(skim.getId()) + "_" + str(skim.getVehicle().getId())+ "_"+ str(skim.getVehicle().getName())+ "_" + int_start_time + "_" + int_duration_time + ".csv"
+    print name
+    _util.exportMatrixCSV(name, skim)
 
 
 '''
-
-
 replication = system.newObject( "GKReplication", model )
 replication.setExperiment(experiment)
-
 print 'created replication'
 cmd3 = model.createNewCmd( model.getType( "GKPathAssignment" ))
 model.getCommander().addCommand( cmd3 )
@@ -87,5 +94,3 @@ GKSystem.getSystem().executeAction( "retrieve", replication, [], "" )
 
 
 model.getCommander().addCommand( None )
-
-
