bc9893e9448a406763aa12788a8979b9c5a6d722
Spencer McNee <spencer.mcnee@gmail.com>
2021-08-04 15:21:40 -0400

    updated OD import with importNetwork.py formats

--- TMGToolbox/input_output/import_matrix.py
+++ TMGToolbox/input_output/import_matrix.py
@@ -1,6 +1,7 @@
 import csv
 from PyANGBasic import *
 from PyANGKernel import *
+from PyANGConsole import *
 from PyANGDTA import *
 from PyMacroKernelPlugin import *
 from PyMacroToolPlugin import *
@@ -9,15 +10,26 @@ from PyMacroPTPlugin import *
 import sys
 import os
 import warnings as _warn
-import sys as _sys
 import traceback as _tb
 import subprocess as _sp
-import sys
-import os
-sys.path.append( xtmf_parameters['toolboxPath'] )
-from common import utilities as _util
-import pandas
-import PySide2.QtCore as qt
+# sys.path.append( xtmf_parameters['toolboxPath'] )
+from datetime import time
+
+# Temporary bridge to get inputs from CSV file instead of xtmf
+argv = sys.argv
+if len(argv) < 4:
+    print("Incorrect Number of Arguments")
+    print("Arguments: -script script.py aimsunNetowrk.ang odMatrix.csv outputNetworkFile.ang")
+xtmf_parameters = {
+    'matrix_location': argv[2],
+    'third_normalized': True,
+    'includes_header': True,
+    'matrix_id': 'TestOD',
+    'centroid_configuration': 'baseCentroidConfig',
+    'vehicle_type': 'Car',
+    'initial_time': '08:00:00:000',
+    'duration_time': '01:00:00:000'
+}
 
 fileLocation = str(xtmf_parameters['matrix_location'])
 thirdNormalized = bool(xtmf_parameters['third_normalized'])
@@ -25,15 +37,25 @@ header = bool(xtmf_parameters['includes_header'])
 matrixId = str(xtmf_parameters['matrix_id'])
 centroidConfigurationId = str(xtmf_parameters['centroid_configuration'])
 vehicleEID = str(xtmf_parameters['vehicle_type'])
-initialTime = str(xtmf_parameters['intial_time'])
+initialTime = str(xtmf_parameters['initial_time'])
 durationTime = str(xtmf_parameters['duration_time'])
 
-system = GKSystem.getSystem()
-model = system.getActiveModel()
+# Start a console
+console = ANGConsole()
+# Load a network
+if console.open(argv[1]): 
+    global model
+    model = console.getModel()
+    print("open network")
+else:
+    console.getLog().addError("Cannot load the network")
+    print("cannot load network")
+
 catalog = model.getCatalog()
 #find the centroid configuration
-if catalog.findObjectByExternalId(centroidConfigurationId) is None:
-    raise Exception("The specified centroid configuration '%s' does not exist") %(centroidConfigurationId)
+sectionType = model.getType("GKCentroidConfiguration")
+if catalog.findObjectByExternalId(centroidConfigurationId, sectionType) is None:
+    raise Exception(f"The specified centroid configuration '{centroidConfigurationId}' does not exist")
 else:
     centroidConfiguration = catalog.findObjectByExternalId(centroidConfigurationId)
 
@@ -41,54 +63,66 @@ else:
 if catalog.findObjectByExternalId(matrixId) is not None:
     while catalog.findObjectByExternalId(matrixId) is not None:
         obj = catalog.findObjectByExternalId(matrixId)
-        _util.deleteObject(obj)
+        # _util.deleteObject(obj)
+        cmd = obj.getDelCmd()
+        model.getCommander().addCommand(cmd)
+        model.getCommander().addCommand(None)
 
 
 # Create new matrix
-cmd = model.createNewCmd( model.getType( "GKODMatrix" ))
-model.getCommander().addCommand( cmd )
-matrix = cmd.createdObject()
+matrix = GKSystem.getSystem().newObject("GKODMatrix", model)
 matrix.setExternalId(matrixId)
 matrix.setStoreId( 2 )
 matrix.setStoreType( 0 )
 matrix.setCentroidConfiguration(centroidConfiguration)
 matrix.setValueToAllCells(0.0)
 
-if catalog.findObjectByExternalId(vehicleEID) is None:
-    raise Exception("The specified vehicle type '%s' does not exist") %(vehicleEID)
+sectionType = model.getType("GKVehicle")
+if catalog.findByName(vehicleEID, sectionType) is None:
+    raise Exception(f"The specified vehicle type '{vehicleEID}' does not exist")
 else:
-    vehicleType = catalog.findObjectByExternalId(vehicleEID)
+    vehicleType = catalog.findByName(vehicleEID, sectionType)
     matrix.setVehicle(vehicleType)
 
 initialTime = initialTime.split(":")
-startTime = qt.Qtime(int(initialTime[0]),int(initialTime[1]))
+startTime = time(int(initialTime[0]),int(initialTime[1]),int(initialTime[2]),int(initialTime[3]))
 matrix.setFrom(startTime)
 durationTime = durationTime.split(":")
-matrix.setDuration(GKTimeDuration(int(durationTime[0]), int(durationTime[1])))
+matrix.setDuration(GKTimeDuration(int(durationTime[0]), int(durationTime[1]), int(durationTime[2])))
 
 ##trying to find the "contents" variable. need to figure out whether people will specify demand vehicle, start time, duration. 
 
 #read file and import
 
-with open(filelocation) as csvfile:
+with open(fileLocation) as csvfile:
     reader = csv.reader(csvfile)
     if header == True:
-        reader.next()
+        next(reader)
     for line in reader:
         if thirdNormalized == True:
-            originEID = line[0]
-            destinationEID = line[1]
+            originEID = f"centroid_{line[0]}"
+            destinationEID = f"centroid_{line[1]}"
             value = float(line[2])
-            if catalog.findObjectByExternalId(originEID) is None:
-                raise Exception("The specified centroid '%s' does not exist") %(originEID)
+            sectionType = model.getType("GKCentroid")
+            if catalog.findObjectByExternalId(originEID, sectionType) is None:
+                raise Exception(f"The specified centroid '{originEID}' does not exist")
             else:
-                origin = catalog.findObjectByExternalId(originEID)
-            if catalog.findObjectByExternalId(destinationEID) is None:
-                raise Exception("The specified centroid '%s' does not exist") %(destinationEID)
+                origin = catalog.findObjectByExternalId(originEID, sectionType)
+            if catalog.findObjectByExternalId(destinationEID, sectionType) is None:
+                raise Exception(f"The specified centroid '{destinationEID}' does not exist")
             else:
-                destination = catalog.findObjectByExternalId(destinationEID)
+                destination = catalog.findObjectByExternalId(destinationEID, sectionType)
             matrix.setTrips(origin, destination, value)
         else:
             raise Exception("Functionality has not been implemented yet")
 
-        
+# Save add the matrix to the network file
+folderName = "GKCentroidConfiguration::matrices"
+folder = model.getCreateRootFolder().findFolder( folderName )
+if folder == None:
+    folder = GKSystem.getSystem().createFolder( model.getCreateRootFolder(), folderName )
+folder.append(matrix)
+# Save the network file
+print("Save Network")
+console.save(argv[3])
+
