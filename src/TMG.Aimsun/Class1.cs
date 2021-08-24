b7bcc39d26ef85ff7cda515ac24b8fa9688258a7
amitsandhel <amit.sandhel@gmail.com>
2021-08-24 16:28:42 -0400

    Folder structure reorganized to resemble XTMF2 and allows us to develop and code in parallel.

---
+++ src/TMG.Aimsun/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMG.Aimsun
+{
+    public class Class1
+    {
+        public void TestMe()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
