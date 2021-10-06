9587849fd4393918ea25c49e894c2286b09355fb
Spencer McNee <spencer.mcnee@gmail.com>
2021-10-06 16:26:52 -0400

    created new file for pedestrian import

---
+++ src/TMGToolbox/inputOutput/importPedestrians.py
@@ -0,0 +1,12 @@
+# Load in the required libraries
+import sys
+import time
+from PyANGBasic import *
+from PyANGKernel import *
+from PyANGConsole import *
+
+def main(argv):
+    print("Add pedestrians")
+
+if __name__ == "__main__":
+    main(sys.argv)
