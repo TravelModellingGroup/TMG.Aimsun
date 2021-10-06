4c787409a8b65382d85a19cd29924bfa7e9213e2
Spencer McNee <spencer.mcnee@gmail.com>
2021-10-06 14:45:40 -0400

    added methods to cache values

--- src/TMGToolbox/inputOutput/importTransitNetwork.py
+++ src/TMGToolbox/inputOutput/importTransitNetwork.py
@@ -5,6 +5,7 @@ from PyANGBasic import *
 from PyANGKernel import *
 from PyANGConsole import *
 import shlex
+from importNetwork import loadModel, initializeNodeConnections
 
 def parseArguments(argv):
     if len(argv) < 3:
@@ -15,10 +16,34 @@ def parseArguments(argv):
     outputNetworkFilename = argv[3]
     return inputModel, networkDirectory, outputNetworkFilename
 
+def cacheAllOfTypeByExternalId(typeString, model, catalog):
+    cacheDict = dict()
+    typeObject = model.getType(typeString)
+    objects = catalog.getObjectsByType(typeObject)
+    for o in iter(objects.values()):
+        externalId = o.getExternalId()
+        cacheDict[externalId] = o
+    return cacheDict
+
+def cacheNodeConnections(listOfNodes, listOfSections):
+    nodeConnections = initializeNodeConnections(listOfNodes)
+    for section in listOfSections:
+        fromNode = section.getOrigin()
+        toNode = section.getDestination()
+        nodeConnections[fromNode].add(section)
+        nodeConnections[toNode].add(section)
+    return nodeConnections
+
 def main(argv):
     console = ANGConsole()
     inputModel, networkDir, outputNetworkFilename = parseArguments(argv)
-
+    global model
+    model, catalog, geomodel = loadModel(inputModel, console)
+    networkLayer = geomodel.findLayer("Network")
+    nodes = cacheAllOfTypeByExternalId("GKNode", model, catalog)
+    sections = cacheAllOfTypeByExternalId("GKSection", model, catalog)
+    nodeConnections = cacheNodeConnections(nodes.values(), sections.values())
+    
 
 if __name__ == "__main__":
     main(sys.argv)
