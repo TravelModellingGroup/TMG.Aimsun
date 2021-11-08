853a51648b6553c1f92886c52b91e03af424eeed
amitsandhel <amit.sandhel@gmail.com>
2021-11-08 10:52:22 -0500

    Refactored macroAssignment.py Refactored codebae to work from unittest bridge and terminal. Added a new unittest module to test macroAssignment. Note that the split of road and travel will be done in the next PR cycle upon further clarification.

--- src/TMGToolbox/assignment/macroAssignment.py
+++ src/TMGToolbox/assignment/macroAssignment.py
@@ -12,153 +12,313 @@ import os
 # sys.path.append( xtmf_parameters['toolboxPath'] )
 # from common import utilities as _util
 
-# Brigde for now to run on input files
-argv = sys.argv
-if len(argv) < 3:
-    print("Incorrect number of arguments")
-    print("Arguments: -script script.py aimsunNetowrk.ang outputNetworkFile.ang")
-    raise Exception("Invalid input arguments")
-xtmf_parameters = {
-    'matrix': 'testOD',
-    # placeholder default values
-    'start': 6.0 * 60.0,
-    'duration': 3.0 * 60.0
-}
-console = ANGConsole()
-model = None
-# Load a network
-if console.open(argv[1]): 
-    model = console.getModel()
-    print("Open network")
-else:
-    console.getLog().addError("Cannot load the network")
-    raise Exception("Cannot load network")
+def run_xtmf(parameters, model, console):
+    """
+    A general function called in all python modules called by bridge. Responsible
+    for extracting data and running appropriate functions.
+    """
+    outputNetworkFile = parameters["OutputNetworkFile"]
+    networkDirectory = parameters["ModelDirectory"]
+    _execute(networkDirectory, outputNetworkFile, model, console)
 
-catalog = model.getCatalog()
-system = GKSystem.getSystem()
-# add info from the OD Matrix into a traffic ddemand item which is used in the model
-trafficDemand = GKSystem.getSystem().newObject("GKTrafficDemand", model)
-scheduleDemandItem = GKScheduleDemandItem()
-sectionType = model.getType("GKODMatrix")
-odMatrix = model.getCatalog().findObjectByExternalId(xtmf_parameters["matrix"], sectionType)
-# TODO make these paramters for the scenario length
-scheduleDemandItem.setFrom(int(xtmf_parameters["start"]*60.0))
-scheduleDemandItem.setDuration(int(xtmf_parameters["duration"]*60.0))
-scheduleDemandItem.setTrafficDemandItem(odMatrix)
-trafficDemand.addToSchedule(scheduleDemandItem)
-# add in the transit demand
-scheduleDemandItem = GKScheduleDemandItem()
-sectionType = model.getType("GKODMatrix")
-odMatrix = model.getCatalog().findObjectByExternalId("transitOD", sectionType)
-# TODO make these paramters for the scenario length
-scheduleDemandItem.setFrom(int(xtmf_parameters["start"]*60.0))
-scheduleDemandItem.setDuration(int(xtmf_parameters["duration"]*60.0))
-scheduleDemandItem.setTrafficDemandItem(odMatrix)
-trafficDemand.addToSchedule(scheduleDemandItem)
-# create a PT plan
-ptPlan = GKSystem.getSystem().newObject("GKPublicLinePlan", model)
-ptPlan.setName("Public Transit Plan")
-ptPlan.setExternalId("publicTransitPlan")
-timeTableType = model.getType("GKPublicLineTimeTable")
-timeTables = model.getCatalog().getObjectsByType(timeTableType)
-for timeTable in iter(timeTables.values()):
-    ptPlan.addTimeTable(timeTable)
+def _execute(networkDirectory, outputNetworkFile, inputModel, console):
+    """ 
+    Main execute function to run the simulation 
+    """
+    model = inputModel
 
-# Create the scenario
-cmd = model.createNewCmd( model.getType( "MacroScenario" ))
-model.getCommander().addCommand( cmd )
-scenario = cmd.createdObject()
-scenario.setDemand(trafficDemand)
-scenario.setPublicLinePlan(ptPlan)
-print(f"Create scenario {scenario.getId()}")
+    xtmf_parameters = {
+        'matrix': 'testOD',
+        # placeholder default values
+        'start': 6.0 * 60.0,
+        'duration': 3.0 * 60.0
+    }
 
-cmd1 = model.createNewCmd( model.getType( "GKPathAssignment" ))
-model.getCommander().addCommand( cmd1 )
-PathAssignment = cmd1.createdObject()
+    catalog = model.getCatalog()
+    system = GKSystem.getSystem()
+    # add info from the OD Matrix into a traffic demand item which is used in the model
+    trafficDemand = GKSystem.getSystem().newObject("GKTrafficDemand", model)
+    scheduleDemandItem = GKScheduleDemandItem()
+    sectionType = model.getType("GKODMatrix")
+    odMatrix = model.getCatalog().findObjectByExternalId(xtmf_parameters["matrix"], sectionType)
+    # TODO make these paramters for the scenario length
+    scheduleDemandItem.setFrom(int(xtmf_parameters["start"]*60.0))
+    scheduleDemandItem.setDuration(int(xtmf_parameters["duration"]*60.0))
+    scheduleDemandItem.setTrafficDemandItem(odMatrix)
+    trafficDemand.addToSchedule(scheduleDemandItem)
+    # add in the transit demand
+    scheduleDemandItem = GKScheduleDemandItem()
+    sectionType = model.getType("GKODMatrix")
+    odMatrix = model.getCatalog().findObjectByExternalId("transitOD", sectionType)
+    # TODO make these paramters for the scenario length
+    scheduleDemandItem.setFrom(int(xtmf_parameters["start"]*60.0))
+    scheduleDemandItem.setDuration(int(xtmf_parameters["duration"]*60.0))
+    scheduleDemandItem.setTrafficDemandItem(odMatrix)
+    trafficDemand.addToSchedule(scheduleDemandItem)
+    # create a PT plan
+    ptPlan = GKSystem.getSystem().newObject("GKPublicLinePlan", model)
+    ptPlan.setName("Public Transit Plan")
+    ptPlan.setExternalId("publicTransitPlan")
+    timeTableType = model.getType("GKPublicLineTimeTable")
+    timeTables = model.getCatalog().getObjectsByType(timeTableType)
+    for timeTable in iter(timeTables.values()):
+        ptPlan.addTimeTable(timeTable)
 
-dataToOutput = scenario.getOutputData()
-dataToOutput.setGenerateSkims(1)
+    # Create the scenario
+    cmd = model.createNewCmd( model.getType( "MacroScenario" ))
+    model.getCommander().addCommand( cmd )
+    scenario = cmd.createdObject()
+    scenario.setDemand(trafficDemand)
+    scenario.setPublicLinePlan(ptPlan)
+    print(f"Create scenario {scenario.getId()}")
 
-scenario.setOutputData(dataToOutput)
+    cmd1 = model.createNewCmd( model.getType( "GKPathAssignment" ))
+    model.getCommander().addCommand( cmd1 )
+    PathAssignment = cmd1.createdObject()
 
-experiment = GKSystem.getSystem().newObject( "MacroExperiment", model )
-experiment.setEngine( "FrankWolfe" )
-params = experiment.createParameters()
-params.setMaxIterations ( 50 )
-params.setMaxRelativeGap ( 0.001 )
-params.setFrankWolfeMethod ( CFrankWolfeParams.eNormal )
-experiment.setParameters( params )
+    dataToOutput = scenario.getOutputData()
+    dataToOutput.setGenerateSkims(1)
 
-model.getCatalog().add( experiment )
-experiment.setScenario( scenario )
-experiment.setOutputPathAssignment( PathAssignment )
+    scenario.setOutputData(dataToOutput)
 
-# Create the PT scenario
-cmd = model.createNewCmd( model.getType( "MacroPTScenario" ))
-model.getCommander().addCommand( cmd )
-ptScenario = cmd.createdObject()
-ptScenario.setDemand(trafficDemand)
-ptScenario.setPublicLinePlan(ptPlan)
-ptOutputData = ptScenario.getOutputData()
-ptOutputData.setGenerateSkims(True)
-ptScenario.setOutputData(ptOutputData)
-print(f"Create transit scenario {ptScenario.getId()}")
+    experiment = GKSystem.getSystem().newObject( "MacroExperiment", model )
+    experiment.setEngine( "FrankWolfe" )
+    params = experiment.createParameters()
+    params.setMaxIterations ( 50 )
+    params.setMaxRelativeGap ( 0.001 )
+    params.setFrankWolfeMethod ( CFrankWolfeParams.eNormal )
+    experiment.setParameters( params )
 
-cmd = model.createNewCmd( model.getType( "MacroPTExperiment" ))
-cmd.setScenario( ptScenario )
-cmd.setEngine( "PTFrequencyBased" )
-cmd.setAlgorithm( "AllOrNothing" )
-model.getCommander().addCommand( cmd )
-ptExperiment = cmd.createdObject()
-# # Execute the scenarios
-print("Run road assignment")
-system.executeAction( "execute", experiment, [], "static assignment")
-experiment.getStatsManager().createTrafficState()
-print("Run transit assignment")
-system.executeAction( "execute", ptExperiment, [], "transit assignment")
+    model.getCatalog().add( experiment )
+    experiment.setScenario( scenario )
+    experiment.setOutputPathAssignment( PathAssignment )
 
-# Generate PT Skim Matrices
-skimMatrices = ptExperiment.getOutputData().getSkimMatrices()
-contConfType = model.getType('GKPedestrianCentroidConfiguration')
-ptCentroidConf = model.getCatalog().findObjectByExternalId("ped_baseCentroidConfig", contConfType)
-# Save to the network file
-folderName = "GKCentroidConfiguration::matrices"
-folder = model.getCreateRootFolder().findFolder( folderName )
-if folder is None:
-    folder = GKSystem.getSystem().createFolder( model.getCreateRootFolder(), folderName )
-for matrix in skimMatrices:
-    m = model.getCatalog().find(matrix)
-    # print(m.ensureMatrixData())
-    print(m.getName())
-    print(m.getTotalTrips())
-    m.setExternalId(f"skimMatrix{matrix}")
-    m.setStoreId(1)
-    m.setStoreType(0)
-    m.setCentroidConfiguration(ptCentroidConf)
-    m.setEnableStore(True)
-    folder.append(m)
-    m.storeExternalMatrix()
-folderName = "GKModel::top::scenarios"
-folder = model.getCreateRootFolder().findFolder( folderName )
-if folder is None:
-    folder = GKSystem.getSystem().createFolder( model.getCreateRootFolder(), folderName )
-folder.append(scenario)
-folder.append(ptScenario)
-folder.append(experiment)
-folder.append(ptExperiment)
-folderName = "GKModel::trafficDemand"
-folder = model.getCreateRootFolder().findFolder( folderName )
-if folder is None:
-    folder = GKSystem.getSystem().createFolder( model.getCreateRootFolder(), folderName )
-folder.append(trafficDemand)
-folderName = "GKModel::publicPlans"
-folder = model.getCreateRootFolder().findFolder( folderName )
-if folder is None:
-    folder = GKSystem.getSystem().createFolder( model.getCreateRootFolder(), folderName )
-folder.append(ptPlan)
-# Save the network file
-print("Save network")
-console.save(argv[2])
-print("Finish Save")
-model.getCommander().addCommand( None )
-print("Done")
+    # Create the PT scenario
+    cmd = model.createNewCmd( model.getType( "MacroPTScenario" ))
+    model.getCommander().addCommand( cmd )
+    ptScenario = cmd.createdObject()
+    ptScenario.setDemand(trafficDemand)
+    ptScenario.setPublicLinePlan(ptPlan)
+    ptOutputData = ptScenario.getOutputData()
+    ptOutputData.setGenerateSkims(True)
+    ptScenario.setOutputData(ptOutputData)
+    print(f"Create transit scenario {ptScenario.getId()}")
+
+    cmd = model.createNewCmd( model.getType( "MacroPTExperiment" ))
+    cmd.setScenario( ptScenario )
+    cmd.setEngine( "PTFrequencyBased" )
+    cmd.setAlgorithm( "AllOrNothing" )
+    model.getCommander().addCommand( cmd )
+    ptExperiment = cmd.createdObject()
+    # # Execute the scenarios
+    print("Run road assignment")
+    system.executeAction( "execute", experiment, [], "static assignment")
+    experiment.getStatsManager().createTrafficState()
+    print("Run transit assignment")
+    system.executeAction( "execute", ptExperiment, [], "transit assignment")
+
+    # Generate PT Skim Matrices
+    skimMatrices = ptExperiment.getOutputData().getSkimMatrices()
+    contConfType = model.getType('GKPedestrianCentroidConfiguration')
+    ptCentroidConf = model.getCatalog().findObjectByExternalId("ped_baseCentroidConfig", contConfType)
+    # Save to the network file
+    folderName = "GKCentroidConfiguration::matrices"
+    folder = model.getCreateRootFolder().findFolder( folderName )
+    if folder is None:
+        folder = GKSystem.getSystem().createFolder( model.getCreateRootFolder(), folderName )
+    for matrix in skimMatrices:
+        m = model.getCatalog().find(matrix)
+        # print(m.ensureMatrixData())
+        print(m.getName())
+        print(m.getTotalTrips())
+        m.setExternalId(f"skimMatrix{matrix}")
+        m.setStoreId(1)
+        m.setStoreType(0)
+        m.setCentroidConfiguration(ptCentroidConf)
+        m.setEnableStore(True)
+        folder.append(m)
+        m.storeExternalMatrix()
+    folderName = "GKModel::top::scenarios"
+    folder = model.getCreateRootFolder().findFolder( folderName )
+    if folder is None:
+        folder = GKSystem.getSystem().createFolder( model.getCreateRootFolder(), folderName )
+    folder.append(scenario)
+    folder.append(ptScenario)
+    folder.append(experiment)
+    folder.append(ptExperiment)
+    folderName = "GKModel::trafficDemand"
+    folder = model.getCreateRootFolder().findFolder( folderName )
+    if folder is None:
+        folder = GKSystem.getSystem().createFolder( model.getCreateRootFolder(), folderName )
+    folder.append(trafficDemand)
+    folderName = "GKModel::publicPlans"
+    folder = model.getCreateRootFolder().findFolder( folderName )
+    if folder is None:
+        folder = GKSystem.getSystem().createFolder( model.getCreateRootFolder(), folderName )
+    folder.append(ptPlan)
+    # Save the network file
+    print("Save network")
+    console.save(outputNetworkFile)
+    print("Finish Save")
+    model.getCommander().addCommand( None )
+    print("Done")
+
+def runFromConsole(inputArgs):
+    """
+    This function takes commands from the terminal, creates a console and model to pass
+    to the _execute function
+    """
+    xtmf_parameters = {
+        'matrix': 'testOD',
+        # placeholder default values
+        'start': 6.0 * 60.0,
+        'duration': 3.0 * 60.0
+    }
+    #extract the data from the args
+    inputNetwork = inputArgs[1]
+    outputNetworkFile = inputArgs[2]
+    
+    # Start a console
+    console = ANGConsole()
+
+    # generate a model of the input network
+    model = None
+    # Load a network
+    if console.open(inputNetwork): 
+        model = console.getModel()
+        print("Open network")
+    else:
+        console.getLog().addError("Cannot load the network")
+        raise Exception("Cannot load network")
+    catalog = model.getCatalog()
+    system = GKSystem.getSystem()
+    # add info from the OD Matrix into a traffic demand item which is used in the model
+    trafficDemand = GKSystem.getSystem().newObject("GKTrafficDemand", model)
+    scheduleDemandItem = GKScheduleDemandItem()
+    sectionType = model.getType("GKODMatrix")
+    odMatrix = model.getCatalog().findObjectByExternalId(xtmf_parameters["matrix"], sectionType)
+    # TODO make these paramters for the scenario length
+    scheduleDemandItem.setFrom(int(xtmf_parameters["start"]*60.0))
+    scheduleDemandItem.setDuration(int(xtmf_parameters["duration"]*60.0))
+    scheduleDemandItem.setTrafficDemandItem(odMatrix)
+    trafficDemand.addToSchedule(scheduleDemandItem)
+    # add in the transit demand
+    scheduleDemandItem = GKScheduleDemandItem()
+    sectionType = model.getType("GKODMatrix")
+    odMatrix = model.getCatalog().findObjectByExternalId("transitOD", sectionType)
+    # TODO make these paramters for the scenario length
+    scheduleDemandItem.setFrom(int(xtmf_parameters["start"]*60.0))
+    scheduleDemandItem.setDuration(int(xtmf_parameters["duration"]*60.0))
+    scheduleDemandItem.setTrafficDemandItem(odMatrix)
+    trafficDemand.addToSchedule(scheduleDemandItem)
+    # create a PT plan
+    ptPlan = GKSystem.getSystem().newObject("GKPublicLinePlan", model)
+    ptPlan.setName("Public Transit Plan")
+    ptPlan.setExternalId("publicTransitPlan")
+    timeTableType = model.getType("GKPublicLineTimeTable")
+    timeTables = model.getCatalog().getObjectsByType(timeTableType)
+    for timeTable in iter(timeTables.values()):
+        ptPlan.addTimeTable(timeTable)
+
+    # Create the scenario
+    cmd = model.createNewCmd( model.getType( "MacroScenario" ))
+    model.getCommander().addCommand( cmd )
+    scenario = cmd.createdObject()
+    scenario.setDemand(trafficDemand)
+    scenario.setPublicLinePlan(ptPlan)
+    print(f"Create scenario {scenario.getId()}")
+
+    cmd1 = model.createNewCmd( model.getType( "GKPathAssignment" ))
+    model.getCommander().addCommand( cmd1 )
+    PathAssignment = cmd1.createdObject()
+
+    dataToOutput = scenario.getOutputData()
+    dataToOutput.setGenerateSkims(1)
+
+    scenario.setOutputData(dataToOutput)
+
+    experiment = GKSystem.getSystem().newObject( "MacroExperiment", model )
+    experiment.setEngine( "FrankWolfe" )
+    params = experiment.createParameters()
+    params.setMaxIterations ( 50 )
+    params.setMaxRelativeGap ( 0.001 )
+    params.setFrankWolfeMethod ( CFrankWolfeParams.eNormal )
+    experiment.setParameters( params )
+
+    model.getCatalog().add( experiment )
+    experiment.setScenario( scenario )
+    experiment.setOutputPathAssignment( PathAssignment )
+
+    # Create the PT scenario
+    cmd = model.createNewCmd( model.getType( "MacroPTScenario" ))
+    model.getCommander().addCommand( cmd )
+    ptScenario = cmd.createdObject()
+    ptScenario.setDemand(trafficDemand)
+    ptScenario.setPublicLinePlan(ptPlan)
+    ptOutputData = ptScenario.getOutputData()
+    ptOutputData.setGenerateSkims(True)
+    ptScenario.setOutputData(ptOutputData)
+    print(f"Create transit scenario {ptScenario.getId()}")
+
+    cmd = model.createNewCmd( model.getType( "MacroPTExperiment" ))
+    cmd.setScenario( ptScenario )
+    cmd.setEngine( "PTFrequencyBased" )
+    cmd.setAlgorithm( "AllOrNothing" )
+    model.getCommander().addCommand( cmd )
+    ptExperiment = cmd.createdObject()
+    # # Execute the scenarios
+    print("Run road assignment")
+    system.executeAction( "execute", experiment, [], "static assignment")
+    experiment.getStatsManager().createTrafficState()
+    print("Run transit assignment")
+    system.executeAction( "execute", ptExperiment, [], "transit assignment")
+
+    # Generate PT Skim Matrices
+    skimMatrices = ptExperiment.getOutputData().getSkimMatrices()
+    contConfType = model.getType('GKPedestrianCentroidConfiguration')
+    ptCentroidConf = model.getCatalog().findObjectByExternalId("ped_baseCentroidConfig", contConfType)
+    # Save to the network file
+    folderName = "GKCentroidConfiguration::matrices"
+    folder = model.getCreateRootFolder().findFolder( folderName )
+    if folder is None:
+        folder = GKSystem.getSystem().createFolder( model.getCreateRootFolder(), folderName )
+    for matrix in skimMatrices:
+        m = model.getCatalog().find(matrix)
+        # print(m.ensureMatrixData())
+        print(m.getName())
+        print(m.getTotalTrips())
+        m.setExternalId(f"skimMatrix{matrix}")
+        m.setStoreId(1)
+        m.setStoreType(0)
+        m.setCentroidConfiguration(ptCentroidConf)
+        m.setEnableStore(True)
+        folder.append(m)
+        m.storeExternalMatrix()
+    folderName = "GKModel::top::scenarios"
+    folder = model.getCreateRootFolder().findFolder( folderName )
+    if folder is None:
+        folder = GKSystem.getSystem().createFolder( model.getCreateRootFolder(), folderName )
+    folder.append(scenario)
+    folder.append(ptScenario)
+    folder.append(experiment)
+    folder.append(ptExperiment)
+    folderName = "GKModel::trafficDemand"
+    folder = model.getCreateRootFolder().findFolder( folderName )
+    if folder is None:
+        folder = GKSystem.getSystem().createFolder( model.getCreateRootFolder(), folderName )
+    folder.append(trafficDemand)
+    folderName = "GKModel::publicPlans"
+    folder = model.getCreateRootFolder().findFolder( folderName )
+    if folder is None:
+        folder = GKSystem.getSystem().createFolder( model.getCreateRootFolder(), folderName )
+    folder.append(ptPlan)
+    # Save the network file
+    print("Save network")
+    console.save(outputNetworkFile)
+    print("Finish Save")
+    model.getCommander().addCommand( None )
+    print("Done")
+
+if __name__ == "__main__":
+    # function to parse the command line arguments and run network script
+    runFromConsole(sys.argv)
---
+++ test/TMG.Aimsun.Tests/TestModuleMacroAssignment.cs
@@ -0,0 +1,46 @@
+﻿/*
+    Copyright 2021 Travel Modelling Group, Department of Civil Engineering, University of Toronto
+
+    This file is part of XTMF.
+
+    XTMF is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    XTMF is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with XTMF.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace TMG.Aimsun.Tests
+{
+    [TestClass]
+    public class TestModuleMacroAssignment
+    {
+        [TestMethod]
+        public void RunMacroAssignment()
+        {
+            //change the network
+            string newNetwork = Path.Combine(Helper.TestConfiguration.NetworkFolder, "aimsunFiles\\FrabitztownNetworkWithOd2.ang");
+            Helper.Modeller.SwitchModel(null, newNetwork);
+
+            string modulePath = Path.Combine(Helper.TestConfiguration.ModulePath, "assignment\\macroAssignment.py");
+            string jsonParameters = JsonConvert.SerializeObject(new
+            {
+                OutputNetworkFile = Path.Combine(Helper.TestConfiguration.NetworkFolder, "aimsunFiles\\output\\FrabitztownNetworkWithAssign.ang"),
+                ModelDirectory = Path.Combine(Helper.TestConfiguration.NetworkFolder, "inputFiles\\Frabitztown"),
+                ToolboxInputOutputPath = Path.Combine(Helper.TestConfiguration.NetworkFolder, "src\\TMGToolbox\\inputOutput")
+            });
+            Helper.Modeller.Run(null, modulePath, jsonParameters);
+        }
+    }
+}
