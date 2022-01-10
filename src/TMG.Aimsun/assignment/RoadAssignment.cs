d365f778b9c2df1f3a6ccd171189c8ee5a8a9530
Amit Sandhel <amit.sandhel@gmail.com>
2022-01-10 10:37:33 -0500

    Refactored RoadAssignment tool. (#64)
    * Refactored RoadAssignment tool.
    Cleaned up variable names and  removed dead code and added header information on missing files
    
    * fixed wording on copyright header
    
    * fixed working on copyright header
    
    * word changes for copyright header

--- src/TMG.Aimsun/assignment/RoadAssignment.cs
+++ src/TMG.Aimsun/assignment/RoadAssignment.cs
@@ -1,20 +1,20 @@
 ﻿/*
     Copyright 2022 Travel Modelling Group, Department of Civil Engineering, University of Toronto
 
-    This file is part of XTMF.
+    This file is part of TMGToolbox for Aimsun.
 
-    XTMF is free software: you can redistribute it and/or modify
+    TMGToolbox for Aimsun is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    XTMF is distributed in the hope that it will be useful,
+    TMGToolbox for Aimsun is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with XTMF.  If not, see <http://www.gnu.org/licenses/>.
+    along with TMGToolbox for Aimsun.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 using System;
@@ -28,9 +28,6 @@ namespace TMG.Aimsun.assignment
     public class RoadAssignment : IAimsunTool
     {
         public const string ToolName = "assignment/roadAssignment.py";
-
-        [SubModelInformation(Required = true, Description = "The directory of the network")]
-        public FileLocation NetworkDirectory;
 
         [SubModelInformation(Required = true, Description = "The directory of the Aimsun toolbox")]
         public FileLocation ToolboxDirectory;
@@ -67,8 +64,6 @@ namespace TMG.Aimsun.assignment
             return aimsunController.Run(this, Path.Combine(ToolboxDirectory, ToolName),
                 JsonParameterBuilder.BuildParameters(writer =>
                 {
-                    writer.WritePropertyName("ModelDirectory");
-                    writer.WriteValue(NetworkDirectory.GetFilePath());
                     writer.WritePropertyName("autoDemand");
                     writer.WriteValue(AutoDemand);
                     writer.WritePropertyName("transitDemand");
