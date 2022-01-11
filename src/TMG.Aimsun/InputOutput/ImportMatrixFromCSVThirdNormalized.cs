24d85c97f133248863ee13105ae79b2c27ae4a7e
Amit Sandhel <amit.sandhel@gmail.com>
2022-01-11 10:38:54 -0500

    Edited the copyright headers replaced XTMF with TMGToolbox for Aimsun (#67)
    Added new copyright headers on files where needed

--- src/TMG.Aimsun/InputOutput/ImportMatrixFromCSVThirdNormalized.cs
+++ src/TMG.Aimsun/InputOutput/ImportMatrixFromCSVThirdNormalized.cs
@@ -1,20 +1,20 @@
 ﻿/*
     Copyright 2021 Travel Modelling Group, Department of Civil Engineering, University of Toronto
 
-    This file is part of XTMF.
+    This file is part of MGToolbox for Aimsun.
 
-    XTMF is free software: you can redistribute it and/or modify
+    MGToolbox for Aimsun is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    XTMF is distributed in the hope that it will be useful,
+    MGToolbox for Aimsun is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with XTMF.  If not, see <http://www.gnu.org/licenses/>.
+    along with MGToolbox for Aimsun.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 using System;
