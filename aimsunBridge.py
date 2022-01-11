556de02b78a8c2ef57958baf7b097b3bfca2c195
amitsandhel <amit.sandhel@gmail.com>
2022-01-11 10:31:20 -0500

    Edited the copyright headers replaced XTMF with TMGToolbox for Aimsun Added new copyright headers on files where needed

--- aimsunBridge.py
+++ aimsunBridge.py
@@ -1,20 +1,20 @@
 """
     Copyright 2021 Travel Modelling Group, Department of Civil Engineering, University of Toronto
 
-    This file is part of XTMF.
+    This file is part of TMGToolbox for Aimsun.
 
-    XTMF is free software: you can redistribute it and/or modify
+    TMGToolbox for Aimsun is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    XTMF is distributed in the hope that it will be useful,
+    TMGToolbox for Aimsun is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with XTMF.  If not, see <http://www.gnu.org/licenses/>.
+    along with TMGToolbox for Aimsun.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 from io import StringIO
